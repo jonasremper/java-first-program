16952fffa3859eac0c65f7b26a86f646c60b195e
jonasremper <131915606+jonasremper@users.noreply.github.com>
2023-04-26 23:02:25 +0200

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -1,9 +1,15 @@
 package com.h2;
 
+import java.util.Scanner;
+
 public class BestLoanRates {
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter you name");
+        String name;
+        scanner.nextLine(name);
+        System.out.println("Hello " + name);
     }
 
 }
