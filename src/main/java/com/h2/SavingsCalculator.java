e249273cd332490d360c92c1e1810c3c74388a16
jonasremper <131915606+jonasremper@users.noreply.github.com>
2023-04-27 22:14:31 +0200

    Update SavingsCalculator.java

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -3,4 +3,7 @@ package com.h2;
 public class SavingsCalculator {
     private float[] credits;
     private float[] debits;
+    public SavingsCalculator(float[] credits, float[] debits) {
+        
+    }
 }
