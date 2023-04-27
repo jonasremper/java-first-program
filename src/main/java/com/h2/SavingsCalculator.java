6a5a85ae329755461db675c03e8736c6b3ef09cb
jonasremper <131915606+jonasremper@users.noreply.github.com>
2023-04-27 22:31:17 +0200

    Update SavingsCalculator.java

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -17,7 +17,11 @@ public class SavingsCalculator {
     }
 
     private float sumOfDebits() {
-        return 0.0f;
+        float sum = 0.0f;
+        for(float debit: debits) {
+            sum += debit;
+        }
+        return sum;
     }
 }
 
