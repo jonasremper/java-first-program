14a5f13a31ced463dd63908a1450d75045f5ef0c
jonasremper <131915606+jonasremper@users.noreply.github.com>
2023-04-28 21:05:36 +0200

    update

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -3,6 +3,32 @@ package com.h2;
 public class Utilities {
 
     public static long getLongValue(String in) {
-        return 0L;
+        long out = Long.MIN_VALUE;
+        try {
+            out = Long.parseLong(in);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'long' value. Exeting program.");
+        }
+        return out;
+    }
+
+    public static int getIntValue(String in) {
+        int out = Integer.MIN_VALUE;
+        try {
+            out = Integer.parseInt(in);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'int' value. Exeting program.");
+        }
+        return out;
+    }
+
+    public static float getFloatValue(String in) {
+        float out = Float.MIN_VALUE;
+        try {
+            out = Float.parseFloat(in);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'float' value. Exeting program.");
+        }
+        return out;
     }
 }
