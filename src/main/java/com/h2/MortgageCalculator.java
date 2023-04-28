6db00959561d40e8eb11229700259b2f8c6d1f4a
jonasremper <131915606+jonasremper@users.noreply.github.com>
2023-04-28 14:06:29 +0200

    Update MortgageCalculator.java

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -1,5 +1,7 @@
 package com.h2;
 
+import java.text.DecimalFormat;
+
 public class MortgageCalculator {
     private long loanAmount;
     private int termInYears;
@@ -31,6 +33,7 @@ public class MortgageCalculator {
     }
 
     public String toString() {
-        return "hello";
+        DecimalFormat df = new DecimalFormat("####0.00");
+        return "monthlyPayment: " + df.format(monthlyPayment);
     }
 }
